382fa25a8c36e09bb85e23e280d00ace59c195a9
sheikhsaadsd <sheikhsaadsd@gmail.com>
2022-07-01 15:05:13 +0200

    push the code

---
+++ src/test/java/com/example/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

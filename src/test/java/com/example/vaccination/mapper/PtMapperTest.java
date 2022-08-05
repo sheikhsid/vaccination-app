946b4db68af789d3afd334ac2732ad6a8809a43d
sheikhsaadsd <sheikhsaadsd@gmail.com>
2022-08-05 17:49:25 +0200

    remove code smell

--- src/test/java/com/example/vaccination/mapper/PtMapperTest.java
+++ src/test/java/com/example/vaccination/mapper/PtMapperTest.java
@@ -1,4 +1,4 @@
-package mapper;
+package com.example.vaccination.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;

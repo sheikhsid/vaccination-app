8e1e4d67d8a24ac1fb59913220c0b5be6be93840
sheikhsaadsd <sheikhsaadsd@gmail.com>
2022-08-22 12:36:49 +0200

    remove code smell

--- src/e2e/java/vaccination/PtWebControllerTesCase.java
+++ src/e2e/java/vaccination/PtWebControllerTesCase.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class PtWebController 
+public class PtWebControllerTesCase 
 {
 	
 	private static int port =

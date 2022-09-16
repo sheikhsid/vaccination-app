d1fe215b2a3219b324a1429e74052a8285fcc0a6
sheikhsaadsd <sheikhsaadsd@gmail.com>
2022-09-16 14:50:02 +0200

    remove warnings

--- src/main/java/com/example/vaccination/exceptions/PtIdExce.java
+++ src/main/java/com/example/vaccination/exceptions/PtIdExce.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PtIdExce extends RuntimeException {
 	
-	private static final long serialVersionUID = 1L;
-
 	public PtIdExce(String message) 
 	{
 		super(message);
